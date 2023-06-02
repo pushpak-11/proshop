b7cf8da4f46edb0efd50b70c2a678a16a9dc31aa
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-02 16:01:50 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
