3a91e1c8368e6e69dd6a44d4ca552516a2c06b4b
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-06 18:35:49 +0530

    Get products from database

---
+++ backend/data/users.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Admin aarav',
+    email: 'aarav@admin.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@email.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@email.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
@@ -13,13 +14,9 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
