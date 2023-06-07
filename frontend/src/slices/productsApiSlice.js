9e1130339e0c35d3e63693ae1723b3721f807a01
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-07 22:37:52 +0530

    fecting products from reduxtoolkit

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,21 @@
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } =
+  productsApiSlice;
