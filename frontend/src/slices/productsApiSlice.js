eb29187e609005c5784236972c806fc40de97e76
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-09 19:14:59 +0530

    Loader & Message Components

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,5 +1,5 @@
 import { PRODUCTS_URL } from '../constants';
-import { apiSlice } from './apiSlice';
+// import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
