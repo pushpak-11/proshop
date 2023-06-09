15fb5337f7f439e87442c59ac347d149cb401bda
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-09 19:15:59 +0530

    change in product

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -4,7 +4,7 @@ import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+// import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
