20de957e12d5d6b96edadeefd5e986c89861075b
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-16 13:31:51 +0530

    User Email & Password Validation

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,13 +10,13 @@ const errorHandler = (err, req, res, next) => {
 
   // If Mongoose not found error, set to 404 and change message
   if (err.name === 'CastError' && err.kind === 'ObjectId') {
-    statusCode = 404;
     message = 'Resource not found';
+    statusCode = 404;
   }
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
