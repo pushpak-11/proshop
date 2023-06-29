3a4eb9ceb9f8c9b56d02ae7be2d7380465cf6714
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-29 20:12:27 +0530

    Payment Method

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
       </Route>
     </Route>
   )
