b7d8b4f404209dfa8b34b0a173fe96a9af3243cc
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-06-30 16:47:39 +0530

    Order Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
