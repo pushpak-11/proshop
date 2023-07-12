32c0171e19d3495cc133f2914ea705bc8e59a5c1
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-07-12 12:28:07 +0530

    Admin Route Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
@@ -25,6 +26,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import OrderListScreen from './screens/admin/OrderListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -41,6 +43,10 @@ const router = createBrowserRouter(
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
         <Route path='/order/:id' element={<OrderScreen />} />
         <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
+
+      <Route path='' element={<AdminRoute />}>
+        <Route path='/admin/orderlist' element={<OrderListScreen />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -0,0 +1,5 @@
+const OrderListScreen = () => {
+  return <div>OrderListScreen</div>;
+};
+
+export default OrderListScreen;
