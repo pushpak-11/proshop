84e4b8c1e25463ec0b677f0b12e04ee39bb15122
pushpak-11 <98308258+pushpak-11@users.noreply.github.com>
2023-07-18 12:40:50 +0530

    bug fix of /api/uploads

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('api/upload', uploadRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
