7c7ef3d398777f9a25f9cb2acf7666051b6f35e1
Pushpak Pawar <pushpakpawar11@gmail.com>
2023-08-01 21:34:34 +0530

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,24 +4,24 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123', 10),
     isAdmin: true,
   },
   {
     name: 'Admin aarav',
     email: 'aarav@admin.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123', 10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123', 10),
   },
   {
     name: 'Jane Doe',
     email: 'jane@email.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123', 10),
   },
 ];
 
